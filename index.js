78b1a07344f952c041253baa42b6cd0492b8e2e9
Andreas Kohn <andreas.kohn@gmail.com>
2017-10-25 10:48:26 +0200

    Check the module against our own eslint style guide

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-'use strict';
+/** @module simple-sns-client */
 
 /**
  * Helper client to simplify access to SNS
  */
 class SNSClient {
-
+	/* eslint-disable-line lines-around-comment */
+	/**
+	 * @param {module:aws-sdk.AWS.SNS} sns AWS SNS client
+	 */
 	constructor(sns) {
 		this.sns = sns;
 
@@ -19,7 +22,7 @@ class SNSClient {
 	 * @return {Promise<String>} ARN of the new topic
 	 */
 	createTopic(topic) {
-		return this.sns.createTopic({ Name: topic }).promise()
+		return this.sns.createTopic({Name: topic}).promise()
 			.then(res => res.TopicArn)
 			.catch(e => {
 				throw new Error(`Failed to publish to SNS: ${e.message}`);
@@ -30,6 +33,7 @@ class SNSClient {
 	 * Returns the ARN of formerly created SNS topics. If the ARN is unknown, it is created in SNS.
 	 * 
 	 * @param {String} topic SNS topic for which the ARN is requested
+	 * @return {Promise<String>} ARN of the topic
 	 */
 	getTopicArn(topic) {
 		const topicArn = this.topicArns[topic];
@@ -40,8 +44,8 @@ class SNSClient {
 	 * Publishes a message to SNS
 	 * 
 	 * @param {String} topicArn	ARN of the topic to whicht message should be published 
-	 * @param {Object} msg
-	 * @returns 
+	 * @param {Object} msg message to publish
+	 * @returns {Promise<any>} result of the publish operation
 	 */
 	publish(topicArn, msg) {
 		const params = {
@@ -50,9 +54,9 @@ class SNSClient {
 		};
 
 		return this.sns.publish(params).promise()
-		.catch(e => {
-			throw new Error(`Failed to publish to SNS: ${e.message}`);
-		});
+			.catch(e => {
+				throw new Error(`Failed to publish to SNS: ${e.message}`);
+			});
 	}
 }
 
